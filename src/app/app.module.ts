0a67148f5113fe815b5bcef629574e132f326302
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-10-24 11:07:21 -0300

    rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { WallLoginComponent } from './wall-login/wall-login.component';
+
+const appRoutes: Routes = [
+  {
+    path:'',
+    component: WallLoginComponent
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WallLoginComponent,    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/wall-login/wall-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wall-login',
+  templateUrl: './wall-login.component.html',
+  styleUrls: ['./wall-login.component.css']
+})
+export class WallLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
