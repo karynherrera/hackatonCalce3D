f6c043b2f28f9f2e1e8aa99102340fc96e8333fa
Karyn Herrera <karynherrera@gmail.com>
2018-10-24 17:35:12 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 
 // Formulario
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
-
-
 import { RouterModule, Routes } from '@angular/router';
-
 import { CrearPacienteComponent } from './crear-paciente/crear-paciente.component';
+import { ProfileComponent } from 'src/app/profile/profile.component';
+import { WallLoginComponent } from 'src/app/wall-login/wall-login.component';
 
 const appRoutes: Routes = [
   {
@@ -30,16 +26,14 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     InicioComponent,
-    
-    
+    ProfileComponent,
     CrearPacienteComponent,
+    WallLoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    
-    
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     FormsModule,
