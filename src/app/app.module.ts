20bae2ceea338c421439674dd638f3e23a7f4196
Karyn Herrera <karynherrera@gmail.com>
2018-10-24 12:14:45 -0300

    actualiza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RouterModule, Routes } from '@angular/router';
 import { WallLoginComponent } from './wall-login/wall-login.component';
+import { CrearPacienteComponent } from './crear-paciente/crear-paciente.component';
+import{FormsModule} from '@angular/forms';
 
 const appRoutes: Routes = [
   {
     path:'',
-    component: WallLoginComponent
+    component: WallLoginComponent, 
+
+  },
+  {
+    path:'crearPaciente',
+    component: CrearPacienteComponent, 
+    
   },
 ];
 
@@ -22,6 +30,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
