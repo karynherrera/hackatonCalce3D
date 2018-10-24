bbdc326349189f5b02849327380f59046bc6fa7f
Tammy <tamara.hi.sa@hotmail.com>
2018-10-24 20:55:11 -0300

    se borra tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,13 @@ import { FormLoginComponent } from './form-login/form-login.component';
 // Formulario
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 import { ProfileComponent } from './profile/profile.component';
 import { RouterModule, Routes } from '@angular/router';
 import { CrearPacienteComponent } from './crear-paciente/crear-paciente.component';
 import { HttpClientModule } from '@angular/common/http';
-import { TableService } from './inicio/tabla/table.service';
 
 import { WallLoginComponent } from 'src/app/wall-login/wall-login.component';
-import {MatCardModule} from '@angular/material/card';
 import { InstitutionComponent } from './institution/institution.component';
 const appRoutes: Routes = [
   {
@@ -40,7 +37,8 @@ const appRoutes: Routes = [
     ProfileComponent,
     CrearPacienteComponent,
     WallLoginComponent,
-    FormLoginComponent, InstitutionComponent, 
+    FormLoginComponent,
+    InstitutionComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,10 +47,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FormsModule,
     HttpClientModule,
-    Ng2SmartTableModule,
-    MatCardModule
   ],
-  providers: [TableService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/inicio/tabla/table.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Table {
-    paciente: string;
-    estado: string;
-    amputacion: string;
-    diagnostico: string;
-    comuna: string;
-    institucion: string;
-}
