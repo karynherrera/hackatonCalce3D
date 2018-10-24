bf2d245a7c4bb0129c62a1af66a8c3dd32f08d18
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-10-24 12:10:49 -0300

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { WallLoginComponent } from './wall-login/wall-login.component';
+import { FormLoginComponent } from './form-login/form-login.component';
+
+
 
 const appRoutes: Routes = [
   {
@@ -15,11 +18,14 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    WallLoginComponent,    
+    WallLoginComponent,
+    FormLoginComponent,
+     
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
