a0d20d19f4835c41bc8c185cb73079462118bc4c
Karyn Herrera <karynherrera@gmail.com>
2018-10-24 20:18:27 -0300

    se agrega vista del chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule, Component } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { FormLoginComponent } from './form-login/form-login.component';
+import {MatButtonModule} from '@angular/material/button';
 // Formulario
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -12,6 +13,7 @@ import { ProfileComponent } from 'src/app/profile/profile.component';
 import { WallLoginComponent } from 'src/app/wall-login/wall-login.component';
 import {MatCardModule} from '@angular/material/card';
 import { InstitutionComponent } from './institution/institution.component';
+import { ScannerComponent } from './scanner/scanner.component';
 const appRoutes: Routes = [
   {
     path: 'crearPaciente',
@@ -25,6 +27,13 @@ const appRoutes: Routes = [
     path:'',
     component: WallLoginComponent, 
   }, 
+  {
+    path: 'scanner',
+    component: ScannerComponent,
+  },
+  {
+    path: '**', pathMatch: 'full', redirectTo: ''
+  }
 ];
 
 @NgModule({
@@ -34,7 +43,7 @@ const appRoutes: Routes = [
     ProfileComponent,
     CrearPacienteComponent,
     WallLoginComponent,
-    FormLoginComponent, InstitutionComponent, 
+    FormLoginComponent, InstitutionComponent, ScannerComponent, 
   ],
   imports: [
     BrowserModule,
@@ -42,7 +51,8 @@ const appRoutes: Routes = [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     FormsModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
