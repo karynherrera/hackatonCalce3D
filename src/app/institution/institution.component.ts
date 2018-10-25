9a6a262cbde065d1d0a59ba217e5a9f4c64e5609
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-10-25 10:21:14 -0300

    se filtra por institucion

--- src/app/institution/institution.component.ts
+++ src/app/institution/institution.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 })
 export class InstitutionComponent implements OnInit {
   pacientes: string;
+  pacient=[];
+  pasFil = [];
+  pasFilFirst = [];
   verInicio: Boolean = false;
   @Output() onInicio: EventEmitter<any> = new EventEmitter<any>();
 
@@ -15,13 +18,40 @@ export class InstitutionComponent implements OnInit {
   ngOnInit() {
     // Fetch del JSON entregado
     fetch('../assets/patients_directory.json')
-      .then(res => res.json())
-      .then(pacientes => {
+      .then(response=> response.json())
+      .then(data =>{
+        console.log(data);
+        this.pacient = data;
+        this.filData("Mutual");
+        this.filDataFirst("INRPAC");
+      });      
+      /*.then(pacientes => {
         console.log(pacientes);
-        this.pacientes = pacientes;        
-      });
+        this.pacientes = pacientes;                        
+      });*/
+  }
+  filData(institucion:string){
+    this.pacient.forEach(element => {
+      console.log(element);
+      if(institucion === element.institucion){
+        console.log(element.institucion);  
+        this.pasFil.push(element);            
+      }
+    });
+    console.log(this.pasFil);    
   }
 
+  filDataFirst(institucion:string){
+    this.pacient.forEach(element => {
+      console.log(element);
+      if(institucion === element.institucion){
+        console.log(element.institucion);  
+        this.pasFilFirst.push(element);            
+      }
+    });
+    console.log(this.pasFilFirst);   
+  }
+  
   viewInicio(event){
     this.verInicio = true;
     console.log(this.verInicio);
