db08614a3e3a093a642c4586c7af0db7a564bfb4
CoteVD <majovardon@gmail.com>
2018-10-25 01:33:44 -0300

    Navbar listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,70 +1,69 @@
 
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, Component } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { InicioComponent } from "./inicio/inicio.component";
-import { FormLoginComponent } from "./form-login/form-login.component";
-import { MatButtonModule } from "@angular/material/button";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FormLoginComponent } from './form-login/form-login.component';
+import { MatButtonModule } from '@angular/material/button';
 // Formulario
-import { FormsModule } from "@angular/forms";
-import { ReactiveFormsModule } from "@angular/forms";
-import { RouterModule, Routes } from "@angular/router";
-import { CrearPacienteComponent } from "./crear-paciente/crear-paciente.component";
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { CrearPacienteComponent } from './crear-paciente/crear-paciente.component';
 
-import { NavbarComponent } from "src/app/navbar/navbar.component";
-import { WallLoginComponent } from "src/app/wall-login/wall-login.component";
-import { MatCardModule } from "@angular/material/card";
-import { InstitutionComponent } from "./institution/institution.component";
-import { ProfileComponent } from "./profile/profile.component";
-import { ScannerComponent } from "./scanner/scanner.component";
-import { ComentarioComponent } from "./scanner/comentario/comentario.component";
-import { AddCommentComponent } from "./scanner/add-comment/add-comment.component";
-import { AngularFireStorageModule } from "@angular/fire/storage";
-import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { NavbarComponent } from 'src/app/navbar/navbar.component';
+import { WallLoginComponent } from 'src/app/wall-login/wall-login.component';
+import { MatCardModule } from '@angular/material/card';
+import { InstitutionComponent } from './institution/institution.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ScannerComponent } from './scanner/scanner.component';
+import { ComentarioComponent } from './scanner/comentario/comentario.component';
+import { AddCommentComponent } from './scanner/add-comment/add-comment.component';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import {
   AngularFirestore,
   AngularFirestoreModule
-} from "@angular/fire/firestore";
-import { AngularFireModule } from "@angular/fire";
-import { environment } from "../environments/environment";
-import { ComentsServiceService } from "src/app/services/coments-service.service";
+} from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { ComentsServiceService } from 'src/app/services/coments-service.service';
 
 const appRoutes: Routes = [
   {
 
-    path: "crearPaciente",
+    path: 'crearPaciente',
     component: CrearPacienteComponent
   },
   {
-    path: "Inicio",
+    path: 'Inicio',
     component: InicioComponent
   },
   {
-    path:'',
-    component: WallLoginComponent, 
-  },  
+    path: '',
+    component: WallLoginComponent,
+  },
   {
-    path: "profile",
+    path: 'profile',
     component: ProfileComponent,
   },
   {
-    path: "muro",
+    path: 'muro',
     component: InstitutionComponent
   },
-  
   {
-    path: "principal",
+    path: 'principal',
     component: NavbarComponent
   },
 
   {
-    path: "scanner",
+    path: 'scanner',
     component: ScannerComponent
   },
   {
-    path: "**",
-    pathMatch: "full",
-    redirectTo: ""
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: ''
   }
 ];
 
