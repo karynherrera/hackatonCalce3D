911f75a09de5b57339e9fee9a9d4ab21f666e114
Karyn Herrera <karynherrera@gmail.com>
2018-10-25 01:58:14 -0300

    se hace deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA7orjAuXBhuu2HKsm4PMpAvyIIBIm70Zk",
+    authDomain: "commentsection-30153.firebaseapp.com",
+    databaseURL: "https://commentsection-30153.firebaseio.com",
+    projectId: "commentsection-30153",
+    storageBucket: "commentsection-30153.appspot.com",
+    messagingSenderId: "672192537724"
+
+  }
 };
