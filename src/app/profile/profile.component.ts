3e39e9eac0cfdfeae2c1ec6d38a4b9922a9580b9
CoteVD <majovardon@gmail.com>
2018-10-25 04:16:24 -0300

    JSON

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,16 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  pacientes: string;
   verScanner: Boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // Fetch del JSON entregado
+    fetch('../assets/patients_directory.json')
+      .then(res => res.json())
+      .then(pacientes => {
+        console.log(pacientes);
+        this.pacientes = pacientes;
+      });
   }
 
-  viewScan(event){
+  viewScan(event) {
     this.verScanner = true;
     console.log(this.verScanner);
-    }
+  }
 }
